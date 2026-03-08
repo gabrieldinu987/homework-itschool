e02193229843123513018c44c9c4e9cb4907506d
gabrieldinu987 <gabrieldinu987@gmail.com>
2026-03-08 13:55:12 +0200

    ex10

---
+++ ex_10/ex10.sh
@@ -0,0 +1,8 @@
+#Adaugati in variabila de mediu PATH directorul cu calea ~/bin-itschools/ doar dacă acesta exista. Faceți acest lucru în mod automat de fiecare data cand se porneste o sesiune cu login pentru userul curent. Adaugati un script în acel director și încercați să-l executați de oriunde.
+
+#Se adauga in ~/.profile
+#!/bin/bash
+
+if [ -d "$HOME/bin-itschools" ]; then
+    PATH="$PATH:$HOME/bin-itschools"
+fi
